5a58cd250ae175cb9a96fee3b58d7bdf849c5662
David Porta <51144828+DPorta@users.noreply.github.com>
2020-09-30 16:35:30 -0500

    Creación de clases converter

---
+++ src/main/java/com/pe/shotathome/converters/ProductConverter.java
@@ -0,0 +1,21 @@
+package com.pe.shotathome.converters;
+
+import com.pe.shotathome.dto.ProductDto;
+import com.pe.shotathome.entity.Product;
+
+public class ProductConverter {
+    public static Product DtoToEntity(ProductDto prd) {
+        Product product=new Product();
+        product.setName(prd.getName());
+        product.setPrice(prd.getPrice());
+        product.setDetail(prd.getDetail());
+        return product;
+    }
+    public static ProductDto EntityToDto(Product prd) {
+        ProductDto productDTO=new ProductDto();
+        productDTO.setName(prd.getName());
+        productDTO.setPrice(prd.getPrice());
+        productDTO.setDetail(prd.getDetail());
+        return productDTO;
+    }
+}
---
+++ src/main/java/com/pe/shotathome/dto/StoreDto.java
@@ -0,0 +1,4 @@
+package com.pe.shotathome.dto;
+
+public class StoreDto {
+}
