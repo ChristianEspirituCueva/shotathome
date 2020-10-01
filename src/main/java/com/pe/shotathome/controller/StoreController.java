41385b7b43fd324e89cbd015e64046f1f70a9c2d
parf915 <renzop915@gmail.com>
2020-09-30 22:30:38 -0500

    controllers
    Creación de los controlladores de orden, producto y tienda(store)

---
+++ src/main/java/com/pe/shotathome/controller/StoreController.java
@@ -0,0 +1,58 @@
+package com.pe.shotathome.controller;
+
+
+
+import com.pe.shotathome.entity.Store;
+import com.pe.shotathome.exception.ResourceNotFoundException;
+import com.pe.shotathome.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+public class StoreController {
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @GetMapping("/stores")
+    public Page<Store> getStores(Pageable pageable) {
+        return StoreRepository.findAll(pageable);
+    }
+
+
+    @PostMapping("/stores")
+    public Store createStore(@Valid @RequestBody Store store) {
+        return storeRepository.save(store);
+    }
+
+    @PutMapping("/stores/{storeId}")
+    public Store updateStore(@PathVariable Long storeId,
+                                   @Valid @RequestBody Store storeRequest) {
+        return storeRepository.findById(storeId)
+                .map(store -> {
+                    store.setTitle(storeRequest.getTitle());
+                    store.setDescription(storeRequest.getDescription());
+                    return storeRepository.save(store);
+                }).orElseThrow(() -> new ResourceNotFoundException("Store not found with id " + storeId));
+    }
+
+
+    @DeleteMapping("/stores/{storeId}")
+    public ResponseEntity<?> deleteStore(@PathVariable Long storeId) {
+        return storeRepository.findById(storeId)
+                .map(store -> {
+                    storeRepository.delete(store);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(() -> new ResourceNotFoundException("Store not found with id " + storeId));
+    }
+
+
+
+
+
+
+}
